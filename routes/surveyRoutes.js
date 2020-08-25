a52eda33155dc0ba5a3bf1d9280012dfde16e59e
Omar Mujtaba <omar.mujtaba@gmail.com>
2020-08-25 19:46:42 +1000

    Implement webhook and update database queries for survey responses

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash');
+const { Path } = require('path-parser');
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -7,9 +10,9 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = (app) => {
-    app.get('/api/surveys/thanks', (req, res) => {
+    app.get('/api/surveys/:surveyId/:choice', (req, res) => {
         res.send('Thanks for voting!');
-    })
+    });
 
     app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
         const { title, subject, body, recipients } = req.body;
@@ -34,5 +37,38 @@ module.exports = (app) => {
             res.status(422).send(err);
         }
         
+    });
+
+    app.post('/api/surveys/webhook', (req, res) => {
+        console.log(req.body);
+        const path = Path.createPath('/api/surveys/:surveyId/:choice');
+        
+        _.chain(req.body)
+            .map((event) => {
+                const match = path.test(new URL(event.url).pathname);
+                if (match) {
+                    return {
+                        email: event.email,
+                        surveyId: match.surveyId,
+                        choice: match.choice
+                    }
+                }
+            })
+            .compact()
+            .uniqBy('email', 'surveyId')
+            .each(event => {
+                Survey.updateOne({
+                    _id: event.surveyId,
+                    recipients: {
+                        $elemMatch: { email: event.email, responded: false }
+                    }
+                }, {
+                    $inc: { [event.choice]: 1 },
+                    $set: { 'recipients.$.responded': true },
+                    lastResponded: new Date()
+                }).exec();
+            })
+            .value();
+        res.send({});
     });
 }
